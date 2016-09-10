2b10c4e919e8f7e57390cc56e6e5d7a5277c1ad5
Andre Aubin <andre@orga.42.fr>
2016-09-10 21:24:24 +0200

    hop

--- moisture.py
+++ moisture.py
@@ -19,7 +19,7 @@ smtp_port = 25 # This is the port that your SMTP provider uses
 smtp_sender = "sender@email.com" # This is the FROM email address
 smtp_receivers = ['receiver@email.com'] # This is the TO email address
 
-# The next two variables use triple quotes, these allow us to preserve the line breaks in the string. 
+# The next two variables use triple quotes, these allow us to preserve the line breaks in the string.
 
 # This is the message that will be sent when NO moisture is detected
 
@@ -45,35 +45,39 @@ def sendEmail(smtp_message):
 	try:
 		smtpObj = smtplib.SMTP(smtp_host, smtp_port)
 		smtpObj.login(smtp_username, smtp_password) # If you don't need to login to your smtp provider, simply remove this line
-		smtpObj.sendmail(smtp_sender, smtp_receivers, smtp_message)         
+		smtpObj.sendmail(smtp_sender, smtp_receivers, smtp_message)
 		print "Successfully sent email"
 	except SMTPException:
 		print "Error: unable to send email"
 
 # This is our callback function, this function will be called every time there is a change on the specified GPIO channel, in this example we are using 17
 
-def callback(channel):  
+def callback(channel):
 	if GPIO.input(channel):
 		print "LED off"
-		sendEmail(message_dead)
+		# sendEmail(message_dead)
 	else:
 		print "LED on"
-		sendEmail(message_alive)
+		# sendEmail(message_alive)
 
 # Set our GPIO numbering to BCM
 GPIO.setmode(GPIO.BCM)
 
 # Define the GPIO pin that we have our digital output from our sensor connected to
-channel = 17
+first_channel = 17
+second_channel = 27
 # Set the GPIO pin to an input
-GPIO.setup(channel, GPIO.IN)
+GPIO.setup(first_channel, GPIO.IN)
+GPIO.setup(second_channel, GPIO.IN)
 
 # This line tells our script to keep an eye on our gpio pin and let us know when the pin goes HIGH or LOW
-GPIO.add_event_detect(channel, GPIO.BOTH, bouncetime=300)
+GPIO.add_event_detect(first_channel, GPIO.BOTH, bouncetime=300)
+GPIO.add_event_detect(second_channel, GPIO.BOTH, bouncetime=300)
 # This line asigns a function to the GPIO pin so that when the above line tells us there is a change on the pin, run this function
-GPIO.add_event_callback(channel, callback)
+GPIO.add_event_callback(first_channel, callback)
 
 # This is an infinte loop to keep our script running
 while True:
 	# This line simply tells our script to wait 0.1 of a second, this is so the script doesnt hog all of the CPU
-	time.sleep(0.1)
+	time.sleep(0.3)
+	print "[17]: {0} | [27]: {1}".format(GPIO.input(first_channel), GPIO.input(second_channel))
